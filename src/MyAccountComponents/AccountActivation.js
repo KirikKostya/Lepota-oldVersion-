d16b930f2a5c84d1416ac55a5ebb5a8673c5bec9
Kirik_10 <kirik.kostya@list.ru>
2023-03-15 14:25:14 +0300

    updateActivation/makeModalViews

--- src/MyAccountComponents/AccountActivation.js
+++ src/MyAccountComponents/AccountActivation.js
@@ -1,33 +1,44 @@
 import axios from 'axios'
 import React, {useState} from 'react'
-import { NavLink, useParams, useSearchParams } from 'react-router-dom'
+import { NavLink, useSearchParams } from 'react-router-dom'
+import ErrorModal from './ErrorModal';
 import './Styles/AccountActivation.css'
 
 export default function AccountActivation() {
-    const [changePage, setChangePage] = useState(false);
     const [link, setLink]  = useSearchParams();
+
+    const [errorMessage, setErrorMessage] = useState('');
 
     const activationFunction = () => {
       axios.defaults.withCredentials = true;
       axios.post(`https://api.native-flora.tk/Auth/Activate/${link.get('id')}`)
       .then(res=>{
-        localStorage.setItem('accessToken', res.data.data);
-      })
+          setErrorMessage('Вы активировали аккаунт!')
+          localStorage.setItem('accessToken', res.data.data);
+        })
       .catch(err=>{
-        console.log(err);
-        alert('Что-то пошло не так!');
+        (err.response.status === 400)
+          ? setErrorMessage('Такой пользователь уже существует!')
+            : (err.response.status === 404)
+              ? setErrorMessage('Такого пользователя не существует!')
+                : setErrorMessage('Что-то пошло не так! Проверьте подключение к интернету')
       })
     }
+
   return (
     <div className='containerForActivation'>
         <h1>Подвердите действие на странице</h1>
+        <ErrorModal errorMessage={errorMessage}
+                    setErrorMessage={setErrorMessage}
+                     />
         <div>
             <input type="checkbox" id="cbx" style={{display: 'none'}} />
             <label htmlFor="cbx" 
                    className="check" 
                    onClick={()=>{
-                    setChangePage(!changePage);
-                    activationFunction();
+                    if(Boolean(localStorage.getItem('accessToken'))){
+                      activationFunction();
+                    }
                    }}>
                 <svg viewBox="0 0 18 18" className='svg'>
                     <path className='path' d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z" />
@@ -35,7 +46,6 @@ export default function AccountActivation() {
                 </svg>
             </label>
         </div>
-         <NavLink to='/' className={`NavLink ${changePage || 'hiddenPar'}`}><span>Перейти на главную страницу</span></NavLink>
     </div>
   )
 }
